4b6654910e545f9083c8e011cbd293ce84cc4538
ReaperOnDrugs <ReaperOnDrugs@users.noreply.github.com>
2020-12-31 02:06:47 +0100

    A fresh start
    Designed in figma btw

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-let nav = document.getElementById("nav");
-let selections = document.getElementById("nav-selections");
-
-function openNav(){
-    nav.classList.toggle("navOPEN");
-    selections.classList.toggle("navOPEN");
-    Array.from(selections.children).forEach((e) => {
-        e.classList.toggle("navIcoOP");
-    });
-}
+/*window.onload = function () {
+    let svg = document.getElementById("tesTID").contentDocument;
+    let items = svg.getElementsByClassName("svgClass");
+    items[0].style.setProperty("fill","#ffffff");
+    items[1].style.setProperty("fill","#ffffff");
+    items[2].style.setProperty("fill","#ffffff");
+    items[0].style.setProperty("transition","fill 0.5s linear");
+    items[1].style.setProperty("transition","fill 0.5s linear");
+    items[2].style.setProperty("transition","fill 0.5s linear");
+}*/
