fb30bbc578a12e17fd1b21c73c53e019f5837fd0
ReaperOnDrugs <ReaperOnDrugs@users.noreply.github.com>
2021-01-12 01:51:45 +0100

    Landing Page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,39 @@
-/*window.onload = function () {
-    let svg = document.getElementById("tesTID").contentDocument;
-    let items = svg.getElementsByClassName("svgClass");
-    items[0].style.setProperty("fill","#ffffff");
-    items[1].style.setProperty("fill","#ffffff");
-    items[2].style.setProperty("fill","#ffffff");
-    items[0].style.setProperty("transition","fill 0.5s linear");
-    items[1].style.setProperty("transition","fill 0.5s linear");
-    items[2].style.setProperty("transition","fill 0.5s linear");
-}*/
+let isSc = false;
+let tlanimc = document.getElementById("tlAnimCont").style;
+let nav = document.getElementById("nav");
+let logo = document.getElementById("logoID");
+
+window.onscroll = function () {
+    if (window.scrollY > 2 && isSc == false){
+        isSc = true;
+        tlanimc.setProperty("margin-top","-60vh");
+        nav.style.setProperty("background-color","#00241b");
+
+        let s = logo.getSVGDocument().querySelectorAll(".svgClass");
+        for (let i = 0; i<s.length; i++){
+            s[i].style.setProperty("fill","white");
+        }
+
+        let j = nav.querySelectorAll(".nav-s");
+        for (let i = 0; i<j.length-1; i++){
+            j[i].classList.add("nav-s-D");
+        }
+        j[j.length-1].classList.add("nav-Start-D");
+    }
+    else if (window.scrollY < 2 && isSc == true){
+        isSc = false;
+        tlanimc.setProperty("margin-top","0vh");
+        nav.style.setProperty("background-color","white");
+
+        let s = logo.getSVGDocument().querySelectorAll(".svgClass");
+        for (let i = 0; i<s.length; i++){
+            s[i].style.setProperty("fill","#00241b");
+        }
+
+        let j = nav.querySelectorAll(".nav-s");
+        for (let i = 0; i<j.length-1; i++){
+            j[i].classList.remove("nav-s-D");
+        }
+        j[j.length-1].classList.remove("nav-Start-D");
+    }
+}
